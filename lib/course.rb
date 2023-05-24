6fb0b6633fe55c03b6844582acee25ac1ec07e96
Dani Wilson <danirae.powell@gmail.com>
2023-05-24 10:43:01 -0600

    Add course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,29 @@
 class Course
+    attr_reader     :name,
+                    :capacity,
+                    :students
 
+    attr_accessor :students
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity
+        @students = []
+        @full = false
+    end
+
+    def full?
+        if @students.length == @capacity
+            @full = true
+        else
+            false
+        end
+    end
+    
+    def enroll(student)
+        @students << student
+        return @students
+    end
+    
+    
 end
