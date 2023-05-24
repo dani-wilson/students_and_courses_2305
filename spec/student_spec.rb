1ead6c1a3fa8950f2fef64af6c6e7dba5043a042
Dani Wilson <danirae.powell@gmail.com>
2023-05-24 10:42:21 -0600

    Add tests for student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,30 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+
+    student = Student.new({name: "Morgan", age: 21})
+
+    it 'exists' do
+        expect(student).to be_a(Student)
+    end
+
+    it 'has attributes' do
+        expect(student.name).to eq("Morgan")
+        expect(student.age).to eq(21)
+    end
+
+    it 'keeps track of student scores' do
+        expect(student.scores).to eq([])
+    end
+
+    it 'logs the student scores' do
+        student.log_score(89)
+        student.log_score(78)
+
+        expect(student.scores).to eq([89, 78])
+    end
+
+    it 'averages all scores' do
+        expect(student.grade).to eq(83.5)
+    end
 end
