2bc62081dde5bd89b15c213c32560d5c8a1ee72f
Dani Wilson <danirae.powell@gmail.com>
2023-05-24 10:42:40 -0600

    Add student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
 class Student
- 
+    attr_reader     :name,
+                    :age,
+                    :scores
+
+    attr_accessor   :grade
+
+    def initialize(student_information)
+        @name = student_information[:name]
+        @age = student_information[:age]
+        @scores = []
+        @grade = grade
+    end
+
+    def log_score(score)
+        @scores << score
+    end
+
+    def grade
+        sum = 0
+        @scores.each do |score|
+            sum += score
+        end
+        grade = sum.to_f / @scores.length
+        return grade
+    end
+
+
 end
